fdb6f96f0ca2a8ad07370770899e0d0f46362479
nani-122 <92074957+nani-122@users.noreply.github.com>
2021-10-21 10:44:18 +0530

    Add files via upload

---
+++ My Details.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	printf("Roll Number:20MH5a0114\n");
+	printf("Name: Anand Tumarada\n");
+	printf("Branch: Civil Engineering\n");
+	printf("Collage:Aditya Collage of Emgineering");
+}
