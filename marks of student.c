25ed1c522df7de3f6d69109ed782b3b10c98c02d
nani-122 <92074957+nani-122@users.noreply.github.com>
2021-10-28 10:48:48 +0530

    Add files via upload

---
+++ marks of student.c
@@ -0,0 +1,20 @@
+/*student marks in a test*/
+#include<stdio.h>
+void main()
+{
+	//vaiable declaration
+	int maths,physics,chemistry,english,Total;
+	//initilization
+	maths=85;
+	physics=45;
+	chemistry=45;
+	english=99;
+	//calculation
+	Total=maths+physics+chemistry+english;
+	//output
+	printf("Marks in maths:%d\n",maths);
+	printf("Marks in physics:%d\n",physics);
+	printf("Marks in chemistry:%d\n",chemistry);
+	printf("Marks in english:%d\n",english);
+	printf("totol marks:%d\n",Total);
+}
